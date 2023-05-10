73f96dcbd3b1bedb03aac408e0bed2c02cec71b8
vicarb <108682674+vicarb@users.noreply.github.com>
2023-05-10 15:23:57 -0400

    Update BookingForm.tsx availability check

--- components/BookingForm/BookingForm.tsx
+++ components/BookingForm/BookingForm.tsx
@@ -1,54 +1,91 @@
 'use client'
 import { useState, useEffect } from 'react';
 import axios, { AxiosResponse } from 'axios';
+import DatePicker from 'react-datepicker';
+import { formatISO } from 'date-fns';
+import 'react-datepicker/dist/react-datepicker.css';
+
 
 interface BookingFormProps {
     services: string[];
   }
   
 const BookingForm: React.FC<BookingFormProps> = ({ services }) => {
-    const [selectedService, setSelectedService] = useState('');
-    const [date, setDate] = useState('');
-    const [time, setTime] = useState('');
-    const [availableHours, setAvailableHours] = useState<{ time: string; isAvailable: boolean }[]>([]);
+  const [selectedService, setSelectedService] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+  const [availableHours, setAvailableHours] = useState<{ time: string; isAvailable: boolean }[]>([]);
+  const [servicesData, setServicesData] = useState<Array<{ _id: string; name: string; description: string; duration: number }>>([]);
 
-  
-    const workingHours = ['09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00'];
-  
-    useEffect(() => {
-        if (date) {
-          const bookedHours: string[] = ['10:00', '13:00', '15:00']; // Mock data for unavailable hours
-      
-          const hours = workingHours.map((hour) => {
-            return {
-              time: hour,
-              isAvailable: !bookedHours.includes(hour),
-            };
-          });
-      
-          setAvailableHours(hours);
-        } else {
-          setAvailableHours([]);
-        }
-      }, [date]);
-      
-      
-      
-      
-  
-      const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-      
+
+
+  useEffect(() => {
+    fetchServices();
+  }, []);
+
+  const fetchServices = async () => {
+    try {
+      const response: AxiosResponse = await axios.get('http://localhost:3000/catalogue');
+      setServicesData(response.data);
+    } catch (error) {
+      console.error('Error fetching services:', error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchTimeSlots = async () => {
+      if (date) {
+        const formattedDate = formatISO(date, { representation: 'date' });
+        console.log("format",formattedDate);
+        
         try {
-          await axios.post('/api/appointments', { selectedService, date, time });
-          alert('Appointment created');
-          setDate('');
-          setTime('');
+          const response: AxiosResponse = await axios.get(
+            `http://localhost:3000/appointments/time-slots?date=${formattedDate}`,
+          );
+    
+          setAvailableHours(response.data);
         } catch (error) {
-          console.error('Error creating appointment:', error);
-          alert('Failed to create appointment');
+          console.error('Error fetching time slots:', error);
         }
-      };
+      } else {
+        setAvailableHours([]);
+      }
+    };
+    console.log("this is date",date);
+    
+    fetchTimeSlots();
+  }, [date]);
+  
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  
+    if (date) {
+      const formattedDate = formatISO(date, { representation: 'date' });
+  
+      try {
+        await axios.post('http://localhost:3000/appointments', { selectedService, date: formattedDate, time });
+        alert('Appointment created');
+        setDate(null);
+        setTime('');
+      } catch (error) {
+        console.error('Error creating appointment:', error);
+        alert('Failed to create appointment');
+      }
+    }
+  };
+  
+  const handleTimeChange = (e) => {
+    const selectedTime = e.target.value;
+    const selectedSlot = availableHours.find(hour => hour.time === selectedTime);
+    if (selectedSlot && !selectedSlot.isAvailable) {
+      alert('This time slot is not available. Please select another.');
+      setTime(''); // reset selection
+    } else {
+      setTime(selectedTime);
+    }
+  };
+      
       
 
   return (
@@ -56,35 +93,38 @@ const BookingForm: React.FC<BookingFormProps> = ({ services }) => {
   <h2 className="text-2xl font-semibold mb-4">Book an appointment</h2>
 
   <div className="mb-4">
+  
     <label htmlFor="service" className="block text-sm font-medium text-gray-700">
       Service
     </label>
     <select
-      id="service"
-      value={selectedService}
-      onChange={(e) => setSelectedService(e.target.value)}
-      className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-    >
-      <option value="">Select a service</option>
-      {services.map((service, index) => (
-        <option key={index} value={service}>
-          {service}
-        </option>
-      ))}
-    </select>
+        id="service"
+        value={selectedService}
+        onChange={(e) => setSelectedService(e.target.value)}
+        className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+      >
+        <option value="">Select a service</option>
+        {servicesData.map((service) => (
+          <option key={service._id} value={service.name}>
+            {service.name}
+          </option>
+        ))}
+
+      </select>
   </div>
+  
 
   <div className="mb-4">
     <label htmlFor="date" className="block text-sm font-medium text-gray-700">
       Date
     </label>
-    <input
-      type="date"
-      id="date"
-      value={date}
-      onChange={(e) => setDate(e.target.value)}
-      className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-    />
+    <DatePicker
+  selected={date}
+  onChange={(date: Date | null) => setDate(date)}
+  dateFormat="yyyy-MM-dd"
+  minDate={new Date()}
+  className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+/>
   </div>
 
   {date && (
@@ -95,12 +135,12 @@ const BookingForm: React.FC<BookingFormProps> = ({ services }) => {
       <select
   id="time"
   value={time}
-  onChange={(e) => setTime(e.target.value)}
+  onChange={handleTimeChange}
   className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
 >
   <option value="">Select an available hour</option>
   {availableHours.map((hour, index) => (
-    <option key={index} value={hour.time} disabled={!hour.isAvailable} style={{ color: hour.isAvailable ? 'inherit' : 'gray' }}>
+    <option key={index} value={hour.time} style={{ color: hour.isAvailable ? 'inherit' : 'gray' }}>
       {hour.time} {hour.isAvailable ? '' : '(unavailable)'}
     </option>
   ))}
