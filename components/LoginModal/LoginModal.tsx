5cb692519b7834a4cdce3afe9b5492d355529d56
vicarb <vcardenasb@protonmail.com>
2023-05-18 19:34:15 -0400

    updated login modal

--- components/LoginModal/LoginModal.tsx
+++ components/LoginModal/LoginModal.tsx
@@ -48,60 +48,59 @@ const LoginModal: React.FC<LoginModalProps> = ({ isOpen, onClose, onLoginSuccess
 
   return (
     <Transition appear show={isOpen} as={Fragment}>
-    <Dialog
+  <Dialog
     as="div"
     className="fixed inset-0 z-10 overflow-y-auto"
     onClose={onClose}
   >
     <div className="flex items-center justify-center min-h-screen px-4 text-center">
       <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
-  
+
       <span
         className="inline-block h-screen align-middle"
         aria-hidden="true"
       >
         &#8203;
       </span>
-      <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl flex flex-col items-center">
+      <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl flex flex-col items-center space-y-6">
         <Dialog.Title
           as="h2"
           className="text-3xl leading-6 font-medium text-gray-900 mb-4"
         >
           Login to your account
         </Dialog.Title>
-        <form className="mt-8 space-y-6 w-full" onSubmit={handleSubmit}>
-          <input type="hidden" name="remember" value="true" />
-          <div className="rounded-md shadow-sm -space-y-px">
+        <form className="w-full" onSubmit={handleSubmit}>
+          <div className="rounded-md shadow-sm space-y-4">
             <div>
-              <label htmlFor="username" className="sr-only">Username</label>
+              <label htmlFor="username" className="text-lg font-medium text-gray-700 mb-1">Username</label>
               <input 
                 id="username"
                 name="username"
                 type="text"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Username"
+                className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                placeholder="Enter your username"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
               />
             </div>
             <div>
-              <label htmlFor="password" className="sr-only">Password</label>
+              <label htmlFor="password" className="text-lg font-medium text-gray-700 mb-1">Password</label>
               <input 
                 id="password"
                 name="password"
                 type="password"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Password"
+                className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                placeholder="Enter your password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
           </div>
-  
-          <div>
-            <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+
+          <div className="mt-4">
+            <button type="submit" className="w-full py-2 px-4 bg-blue-600 text-white font-semibold rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
               Sign in
             </button>
           </div>
@@ -110,6 +109,7 @@ const LoginModal: React.FC<LoginModalProps> = ({ isOpen, onClose, onLoginSuccess
     </div>
   </Dialog>
 </Transition>
+
 
   )
 
