d7af096bac5dd8495de3b220eebbe7d63c799457
vicarb <108682674+vicarb@users.noreply.github.com>
2023-05-20 16:03:58 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 import Image from 'next/image';
 import BookingForm from '@/components/BookingForm/BookingForm';
 import Head from 'next/head';
-import Dashboard from '@/components/Dashboard/Dashboard';
 import { Register } from '@/components/Register/Register';
 import { Login } from '@/components/Login/Login';
 import Landing from '@/components/Landing/Landing';
@@ -22,7 +21,7 @@ export default function Home() {
 
       <Landing/>
       <BookingForm  />
-      <Dashboard/>
+      
       <Login/>
       <Register/>
     </div>
