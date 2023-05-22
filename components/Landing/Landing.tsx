11ae169a62d09e31796890bfa56b14d4b378f47e
vicarb <vcardenasb@protonmail.com>
2023-05-22 15:44:37 -0400

    updated landing layout

--- components/Landing/Landing.tsx
+++ components/Landing/Landing.tsx
@@ -121,89 +121,87 @@ const Landing = () => {
     }
   };
   return (
-    <>
     <div className="min-h-screen bg-gray-200 py-6 flex flex-col justify-center sm:py-12">
-        <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-            <div className="absolute inset-0 bg-gradient-to-r from-blue-400 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-            <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-                <header className="text-4xl font-bold text-blue-500 mb-8 text-center">
-                    Welcome to Our Service Booking Website!
-                </header>
-                
-                <form onSubmit={handleSubmit} className="w-full">
-                <LoginModal 
-                    isOpen={isLoginModalOpen} 
-                    onClose={closeLoginModal} 
-                    onLoginSuccess={handleFormSubmitAfterLogin}
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <header className="text-2xl md:text-4xl font-bold text-blue-500 mb-8 text-center">
+            Welcome to Our Service Booking Website!
+          </header>
+          
+          <form onSubmit={handleSubmit} className="w-full">
+            <LoginModal 
+                isOpen={isLoginModalOpen} 
+                onClose={closeLoginModal} 
+                onLoginSuccess={handleFormSubmitAfterLogin}
+            />
+            
+            <div className="mb-4">
+                <label htmlFor="name" className="block text-xl font-semibold text-gray-700 mb-2">
+                    Name
+                </label>
+                <input
+                    id="name"
+                    type="text"
+                    value={customerName}
+                    onChange={(e) => setCustomerName(e.target.value)}
+                    className="mt-1 block w-full py-3 px-4 border border-gray-300 bg-white rounded-md shadow-sm text-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                 />
-                
-                <div className="mb-4">
-                    <label htmlFor="name" className="block text-xl font-semibold text-gray-700 mb-2">
-                        Name
-                    </label>
-                    <input
-                        id="name"
-                        type="text"
-                        value={customerName}
-                        onChange={(e) => setCustomerName(e.target.value)}
-                        className="mt-1 block w-full py-3 px-4 border border-gray-300 bg-white rounded-md shadow-sm text-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-                    />
-                </div>
+            </div>
 
+            <div className="mb-8">
+                <label htmlFor="service" className="block text-xl font-semibold text-gray-700 mb-2">
+                    Service
+                </label>
+                <select
+                    id="service"
+                    value={selectedService}
+                    onChange={(e) => setSelectedService(e.target.value)}
+                    className="mt-1 block w-full py-3 px-4 border border-gray-300 bg-white rounded-md shadow-sm text-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                >
+                    <option value="">Select a service</option>
+                    {servicesData.map((service) => (
+                        <option key={service._id} value={service.name}>
+                            {service.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
+            <div className="mb-8">
+                <label htmlFor="date" className="block text-xl font-semibold text-gray-700 mb-2">
+                    Date
+                </label>
+                <DatePicker
+                    selected={date}
+                    onChange={(date) => setDate(date)}
+                    dateFormat="yyyy-MM-dd"
+                    minDate={new Date()}
+                    className="mt-1 block w-full py-3 px-4 border border-gray-300 bg-white rounded-md shadow-sm text-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                />
+            </div>
+
+            {date && (
                 <div className="mb-8">
-                    <label htmlFor="service" className="block text-xl font-semibold text-gray-700 mb-2">
-                        Service
+                    <label htmlFor="time" className="block text-xl font-semibold text-gray-700 mb-2">
+                        Time
                     </label>
                     <select
-                        id="service"
-                        value={selectedService}
-                        onChange={(e) => setSelectedService(e.target.value)}
+                        id="time"
+                        value={time}
+                        onChange={handleTimeChange}
                         className="mt-1 block w-full py-3 px-4 border border-gray-300 bg-white rounded-md shadow-sm text-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                     >
-                        <option value="">Select a service</option>
-                        {servicesData.map((service) => (
-                            <option key={service._id} value={service.name}>
-                                {service.name}
+                        <option value="">Select an available hour</option>
+                        {availableHours.map((hour, index) => (
+                            <option key={index} value={hour.time} style={{ color: hour.isAvailable ? 'inherit' : 'gray' }}>
+                                {hour.time} {hour.isAvailable ? '' : '(unavailable)'}
                             </option>
                         ))}
                     </select>
                 </div>
+            )}
 
-                <div className="mb-8">
-                    <label htmlFor="date" className="block text-xl font-semibold text-gray-700 mb-2">
-                        Date
-                    </label>
-                    <DatePicker
-                        selected={date}
-                        onChange={(date: Date | null) => setDate(date)}
-                        dateFormat="yyyy-MM-dd"
-                        minDate={new Date()}
-                        className="mt-1 block w-full py-3 px-4 border border-gray-300 bg-white rounded-md shadow-sm text-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-                    />
-                </div>
-
-                {date && (
-                    <div className="mb-8">
-    <label htmlFor="time" className="block text-xl font-semibold text-gray-700 mb-2">
-        Time
-    </label>
-    <select
-        id="time"
-        value={time}
-        onChange={handleTimeChange}
-        className="mt-1 block w-full py-3 px-4 border border-gray-300 bg-white rounded-md shadow-sm text-lg focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-    >
-        <option value="">Select an available hour</option>
-        {availableHours.map((hour, index) => (
-            <option key={index} value={hour.time} style={{ color: hour.isAvailable ? 'inherit' : 'gray' }}>
-                {hour.time} {hour.isAvailable ? '' : '(unavailable)'}
-            </option>
-        ))}
-    </select>
-</div>
-                )}
-
-                <div className="mb-8">
+            <div className="mb-8">
                 <button
                     type="submit"
                     className="w-full py-3 px-4 text-lg font-semibold bg-blue-600 text-white rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
@@ -211,17 +209,11 @@ const Landing = () => {
                     Book appointment
                 </button>
             </div>
-            
-            
-            
-                
-            </form>
-            </div>
+          </form>
         </div>
+      </div>
     </div>
-    
-    </>
-);
+  );
 
 };
 
