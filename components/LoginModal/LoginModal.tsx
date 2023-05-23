092368e76f8595570e18a23410d3e2ca3f768dee
vicarb <vcardenasb@protonmail.com>
2023-05-23 13:24:42 -0400

    updated urls

--- components/LoginModal/LoginModal.tsx
+++ components/LoginModal/LoginModal.tsx
@@ -29,7 +29,7 @@ const LoginModal: React.FC<LoginModalProps> = ({ isOpen, onClose, onLoginSuccess
     e.preventDefault();
   
     try {
-      const response = await axios.post('http://localhost:3000/auth/login', {
+      const response = await axios.post('https://hour-booking-back.vercel.app/auth/login', {
         username,
         password,
       });
