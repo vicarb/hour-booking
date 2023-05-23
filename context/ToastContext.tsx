4dddee848529d8c12a946e29de775eed80dee37e
vicarb <vcardenasb@protonmail.com>
2023-05-23 12:31:03 -0400

    updated toast and added progress bar

--- context/ToastContext.tsx
+++ context/ToastContext.tsx
@@ -22,64 +22,78 @@ type ToastContextProviderProps = {
 };
 
 export const ToastContextProvider = ({ children }: ToastContextProviderProps) => {
-    const [message, setMessage] = useState("");
-    const [type, setType] = useState("");
-    const [show, setShow] = useState(false);
-  
-    const showToast = (message: string, type: string = "success") => {
-      console.log("Show toast called with", { message, type }); 
-      setMessage(message);
-      setType(type);
-      setShow(true);
-    };
-    
-  
-    const hideToast = () => {
-      setShow(false);
-    };
-  
-    useEffect(() => {
-      console.log({ message, type, show }); // Add this line here
-      if (show) {
-        const timer = setTimeout(() => {
-          hideToast();
-        }, 2000);
-        return () => clearTimeout(timer);
-      }
-    }, [show, message, type]);
-  
-    return (
-      <ToastContext.Provider value={{ showToast, hideToast }}>
-        {children}
-        {show && (
-          <div 
-            style={{
-              position: 'fixed', 
-              top: '50%', 
-              left: '50%', 
-              width: '80%', // Use percentage to be responsive
-              maxWidth: '400px', // Limit maximum width
-              transform: 'translate(-50%, -50%)',
-              padding: '1rem', 
-              borderRadius: '0.375rem', 
-              fontSize: '1.5625rem', // 25% larger than previous size
-              backgroundColor: '#10B981', // Tailwind's green-600
-              color: 'white', 
-              boxShadow: '0 25px 50px -12px rgba(0, 0, 0, 0.25)', // Tailwind's shadow-2xl
-              zIndex: 9999,
-              textAlign: 'center', // Center text
-            }}
-          >
-            {message}
-          </div>
-        )}
-        
-        
-        
-        
-        
-        
-        
-      </ToastContext.Provider>
-    );
+  const [message, setMessage] = useState("");
+  const [type, setType] = useState("");
+  const [show, setShow] = useState(false);
+  const [remainingTime, setRemainingTime] = useState(0);
+
+  const showToast = (message: string, type: string = "success") => {
+    setMessage(message);
+    setType(type);
+    setShow(true);
+    setRemainingTime(2000);
   };
+
+  const hideToast = () => {
+    setShow(false);
+    setMessage("");
+    setType("");
+    setRemainingTime(0);
+  };
+
+  useEffect(() => {
+    if (show) {
+      const startTime = Date.now();
+      const timer = setInterval(() => {
+        const elapsedTime = Date.now() - startTime;
+        const updatedRemainingTime = 2000 - elapsedTime;
+        setRemainingTime(updatedRemainingTime);
+        if (updatedRemainingTime <= 0) {
+          hideToast();
+          clearInterval(timer);
+        }
+      }, 20); // Increased interval duration to 20ms
+
+      return () => {
+        clearInterval(timer);
+      };
+    }
+  }, [show]);
+
+  const progress = (remainingTime / 2000) * 100;
+
+  return (
+    <ToastContext.Provider value={{ showToast, hideToast }}>
+      {children}
+      {show && (
+        <div
+          style={{
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            padding: '1rem',
+            borderRadius: '0.375rem',
+            fontSize: '1.5625rem',
+            backgroundColor: '#10B981',
+            color: 'white',
+            boxShadow: '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+            zIndex: 9999,
+            textAlign: 'center',
+          }}
+        >
+          {message}
+          <div
+            style={{
+              width: `${progress}%`,
+              height: '5px',
+              backgroundColor: '#fff',
+              marginTop: '1rem',
+              transition: 'width 100ms ease-out',
+            }}
+          />
+        </div>
+      )}
+    </ToastContext.Provider>
+  );
+};
